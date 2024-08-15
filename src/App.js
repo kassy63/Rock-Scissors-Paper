1d9d54e8de7e0df7407825f06ad3fbc6f7570cc5
kassy <sky122413@gmail.com>
2024-08-15 22:09:03 +0900

    Class 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Box from "./component/Box";
-
-// 1. 박스 2개 (타이틀, 사진, 결과)
-// 2. 가위 바위 보 버튼이 있다
-// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-// 4. 컴퓨터는 랜덤하게 아이템 선택이 된다
-// 5. 3, 4의 결과를 가지고 누가 이겼는지 승패를 따진다
-// 6. 승패 결과에 따라 테두리 색이 바뀐다 (이기면 초록, 지면 빨강, 비기면 검정)
 
 const choice = {
   rock: {
@@ -24,106 +17,93 @@ const choice = {
   },
 };
 
-function App() {
-  const [userSelect, setUserSelect] = useState(null);
-  const [computerSelect, setComputerSelect] = useState(null);
-  const [result, setResult] = useState("");
-  const [selectedButton, setSelectedButton] = useState("");
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userSelect: null,
+      computerSelect: null,
+      result: "",
+      selectedButton: "",
+    };
+  }
 
-  // const play = (userChoice) => {
-  //   setUserSelect(choice[userChoice]);
-  //   const computerChoice = randomChoice();
-  //   setComputerSelect(computerChoice);
-  //   setResult(judgement(choice[userChoice], computerChoice));
-  // };
-  const play = (userChoice) => {
+  play = (userChoice) => {
     const user = choice[userChoice];
-    const computer = randomChoice();
-    const gameResult = judgement(user, computer);
+    const computer = this.randomChoice();
+    const gameResult = this.judgement(user, computer);
 
-    setUserSelect(user);
-    setComputerSelect(computer);
-    setResult(gameResult);
-    setSelectedButton(userChoice);
+    this.setState({
+      userSelect: user,
+      computerSelect: computer,
+      result: gameResult,
+      selectedButton: userChoice,
+    });
   };
 
-  const judgement = (user, computer) => {
-    console.log("~ judgement ~ user, computer:", user, computer);
-
-    // user === computer tie
-    // user === rock, computer === scissors user WIN
-    // user === rock, computer === paper user LOSE
-    // user === scissors, computer === paper user WIN
-    // user === scissors, computer === rock user LOSE
-    // user === paper, computer === rock user WIN
-    // user === paper, computer === scissors user LOSE
-
+  judgement = (user, computer) => {
     if (user.name === computer.name) {
       return "TIE";
-    } else if (user.name === "Rock")
+    } else if (user.name === "Rock") {
       return computer.name === "Scissors" ? "WIN" : "LOSE";
-    else if (user.name === "Scissors")
+    } else if (user.name === "Scissors") {
       return computer.name === "Paper" ? "WIN" : "LOSE";
-    else if (user.name === "Paper")
+    } else if (user.name === "Paper") {
       return computer.name === "Rock" ? "WIN" : "LOSE";
+    }
   };
 
-  // const randomChoice = () => {
-  //   const itemArray = Object.keys(choice);
-  //   console.log("~ randomChoice ~ itemArray:", itemArray);
-  //   const randomItem = Math.floor(Math.random() * itemArray.length);
-  //   console.log("~ randomChoice ~ randomItem:", randomItem);
-  //   const final = itemArray[randomItem];
-  //   console.log("~ randomChoice ~ final:", final);
-  //   return choice[final];
-  // };
-  const randomChoice = () => {
-    const itemArray = Object.keys(choice); // Object.keys() -> 객체에 키 값만 뽑아서 어레이로 만들어주는 함수
+  randomChoice = () => {
+    const itemArray = Object.keys(choice);
     const randomItem = Math.floor(Math.random() * itemArray.length);
     return choice[itemArray[randomItem]];
   };
 
-  return (
-    <div className="wrapper">
-      <h1>가위바위보 게임</h1>
-      <div className="main">
-        <Box
-          title="You"
-          item={userSelect}
-          result={
-            result === "WIN" ? "WIN" : result === "LOSE" ? "LOSE" : result
-          }
-        />
-        <Box
-          title="Computer"
-          item={computerSelect}
-          result={
-            result === "LOSE" ? "WIN" : result === "WIN" ? "LOSE" : result
-          }
-        />
+  render() {
+    const { userSelect, computerSelect, result, selectedButton } = this.state;
+
+    return (
+      <div className="wrapper">
+        <h1>가위바위보 게임</h1>
+        <div className="main">
+          <Box
+            title="You"
+            item={userSelect}
+            result={
+              result === "WIN" ? "WIN" : result === "LOSE" ? "LOSE" : result
+            }
+          />
+          <Box
+            title="Computer"
+            item={computerSelect}
+            result={
+              result === "LOSE" ? "WIN" : result === "WIN" ? "LOSE" : result
+            }
+          />
+        </div>
+        <div className="buttons">
+          <button
+            onClick={() => this.play("scissors")}
+            className={selectedButton === "scissors" ? "selected" : ""}
+          >
+            <img src={choice.scissors.img} alt="Scissors" />
+          </button>
+          <button
+            onClick={() => this.play("rock")}
+            className={selectedButton === "rock" ? "selected" : ""}
+          >
+            <img src={choice.rock.img} alt="Rock" />
+          </button>
+          <button
+            onClick={() => this.play("paper")}
+            className={selectedButton === "paper" ? "selected" : ""}
+          >
+            <img src={choice.paper.img} alt="Paper" />
+          </button>
+        </div>
       </div>
-      <div className="buttons">
-        <button
-          onClick={() => play("scissors")}
-          className={selectedButton === "scissors" ? "selected" : ""}
-        >
-          <img src={choice.scissors.img} alt="Scissors" />
-        </button>
-        <button
-          onClick={() => play("rock")}
-          className={selectedButton === "rock" ? "selected" : ""}
-        >
-          <img src={choice.rock.img} alt="Rock" />
-        </button>
-        <button
-          onClick={() => play("paper")}
-          className={selectedButton === "paper" ? "selected" : ""}
-        >
-          <img src={choice.paper.img} alt="Paper" />
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,46 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Box = (props) => {
-  const getBorderColor = () => {
-    if (props.result === "WIN") return "green";
-    else if (props.result === "LOSE") return "red";
+class Box extends Component {
+  getBorderColor = () => {
+    const { result } = this.props;
+    if (result === "WIN") return "green";
+    else if (result === "LOSE") return "red";
     else return "black";
   };
 
-  return (
-    <div
-      className="box"
-      style={{
-        border: `5px solid ${getBorderColor()}`,
-      }}
-    >
-      {props.item ? (
-        <>
-          <h1
-            style={{
-              color: `${getBorderColor()}`,
-            }}
-          >
-            {props.title}
-          </h1>
-          <img
-            className="item-img"
-            src={props.item && props.item.img}
-            alt={props.item.name}
-          />
-        </>
-      ) : (
-        <h1>{props.title}</h1>
-      )}
-      <h2
+  render() {
+    const { title, item, result } = this.props;
+    const borderColor = this.getBorderColor();
+
+    return (
+      <div
+        className="box"
         style={{
-          color: `${getBorderColor()}`,
+          border: `5px solid ${borderColor}`,
         }}
       >
-        {props.result}
-      </h2>
-    </div>
-  );
-};
+        {item ? (
+          <>
+            <h1
+              style={{
+                color: borderColor,
+              }}
+            >
+              {title}
+            </h1>
+            <img className="item-img" src={item.img} alt={item.name} />
+          </>
+        ) : (
+          <h1>{title}</h1>
+        )}
+        <h2
+          style={{
+            color: borderColor,
+          }}
+        >
+          {result}
+        </h2>
+      </div>
+    );
+  }
+}
 
 export default Box;
